aead117b6a89a53a0005030fb82240fbb19948fd
LDQ-first <178848890@qq.com>
2017-08-30 11:05:49 +0800

    modify

--- pages/hot/hot.js
+++ pages/hot/hot.js
@@ -14,7 +14,30 @@ Page({
         return API.getHot()
                   .then(subjects => {
                       console.log('hotSubjects: ', subjects)
-                      
+                      let hotArr = []
+                      subjects.forEach((obj, idx) => {
+                          let newObj = {}
+                          newObj.title = obj.title
+                          newObj.id = obj.id
+                          newObj.average = obj.rating.average
+                          newObj.stars = obj.rating.stars
+                          newObj.year = obj.year
+                          newObj.img = obj.images.large
+                          console.log(newObj)
+                          hotArr.push(newObj)
+                      })
+                      this.setData({
+                          subjects: subjects,
+                          hot: hotArr
+                      })
                   })
+                  .catch(this.onError)
+    },
+    onError (err) {
+        wx.showToast({
+            icon: 'loading',
+            title: err.msg + '',
+            duration: 2000
+        })
     }
 })
